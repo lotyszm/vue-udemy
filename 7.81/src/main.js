c221a2efe48a41206a756528dd15b7c8676022f0
Maciej Łotysz <maciejlotysz@gmail.com>
2024-05-06 13:15:46 +0200

    day 9

---
+++ 7.81/src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import FriendContact from './components/FriendContact.vue'
+
+createApp(App)
+  .component('friend-contact', FriendContact)
+  .mount('#app');
